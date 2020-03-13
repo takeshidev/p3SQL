f5ccfcbeaf023b66461a2b164969707fcce58bc7
takeshidev <59844178+takeshidev@users.noreply.github.com>
2020-03-13 17:29:59 -0300

    17:29

---
+++ P3C_CANTIDAD_ALTERNATIVAS.sql
@@ -0,0 +1,7 @@
+--------------------------------------------------------
+--  DDL for View P3C_CANTIDAD_ALTERNATIVAS
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE VIEW "EVALUACION3"."P3C_CANTIDAD_ALTERNATIVAS" ("IDPREGUNTA", "CANTIDAD_ALTERNATIVAS", "CANTIDAD_CORRECTAS") AS 
+  select pregunta.idpregunta, count(alternativa.idalternativa) as Cantidad_alternativas, sum(alternativa.correcta) as cantidad_correctas from pregunta join alternativa on alternativa.idpregunta = pregunta.idpregunta having sum(alternativa.correcta)=0 or sum(alternativa.correcta)=count(alternativa.idalternativa)  group by pregunta.idpregunta order by idpregunta
+;
