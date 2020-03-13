efba16d86e3232d9b5ab2bb07a6309998bd4a4e6
takeshidev <59844178+takeshidev@users.noreply.github.com>
2020-03-13 17:10:56 -0300

    17:10

---
+++ P3B_MENOS_2_ALTERNATIVAS.sql
@@ -0,0 +1,12 @@
+--------------------------------------------------------
+--  DDL for View P3B_MENOS_2_ALTERNATIVAS
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE VIEW "EVALUACION3"."P3B_MENOS_2_ALTERNATIVAS" ("NOMBRE", "IDPREGUNTA", "NUM_ALTERNATIVAS") AS 
+  select evaluacion.nombre, pregunta.idpregunta, count(alternativa.idalternativa) as num_alternativas
+from evaluacion
+join pregunta on pregunta.idevaluacion = evaluacion.idevaluacion
+join alternativa
+on alternativa.idpregunta = pregunta.idpregunta having count(alternativa.idalternativa)<3
+group by evaluacion.nombre, pregunta.idpregunta
+;
